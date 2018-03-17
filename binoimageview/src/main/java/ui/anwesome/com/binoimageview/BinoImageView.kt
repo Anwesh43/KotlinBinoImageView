05c9d4369dcfc0fa4a22e86ef36191ca05c8a82c
Anwesh43 <anweshthecool0@gmail.com>
2018-03-17 09:12:36 +0530

    adding BinoImageView class

--- binoimageview/src/main/java/ui/anwesome/com/binoimageview/BinoImageView.kt
+++ binoimageview/src/main/java/ui/anwesome/com/binoimageview/BinoImageView.kt
@@ -3,3 +3,19 @@ package ui.anwesome.com.binoimageview
 /**
  * Created by anweshmishra on 17/03/18.
  */
+import android.view.*
+import android.content.*
+import android.graphics.*
+class BinoImageView(ctx : Context) : View(ctx) {
+    override fun onDraw(canvas : Canvas) {
+
+    }
+    override fun onTouchEvent(event : MotionEvent):Boolean {
+        when(event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
