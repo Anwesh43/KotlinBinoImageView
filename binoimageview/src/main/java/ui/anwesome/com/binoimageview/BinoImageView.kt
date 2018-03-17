e27fbbd973477507ce446ee9cea0997d25a5e6eb
Anwesh43 <anweshthecool0@gmail.com>
2018-03-17 09:22:44 +0530

    adding State class

--- binoimageview/src/main/java/ui/anwesome/com/binoimageview/BinoImageView.kt
+++ binoimageview/src/main/java/ui/anwesome/com/binoimageview/BinoImageView.kt
@@ -18,4 +18,27 @@ class BinoImageView(ctx : Context) : View(ctx) {
         }
         return true
     }
+    data class State(var prevScale : Float = 0f, var dir : Float = 0f, var j : Int = 0, var jDir : Int = 1) {
+        val scales : Array<Float> = arrayOf(0f, 0f)
+        fun update(stopcb : (Float) -> Unit) {
+            scales[j] += 0.1f * dir
+            if(Math.abs(scales[j] - prevScale) > 1) {
+                scales[j] = prevScale + dir
+                j += jDir
+                if (j == scales.size || j == -1) {
+                    jDir *= -1
+                    j += jDir
+                    scales[j] = prevScale
+                    dir = 0f
+                    stopcb(scales[j])
+                }
+            }
+        }
+        fun startUpdating(startcb : () -> Unit) {
+            if (dir == 0f) {
+                dir = 1 - 2 * prevScale
+                startcb()
+            }
+        }
+    }
 }
