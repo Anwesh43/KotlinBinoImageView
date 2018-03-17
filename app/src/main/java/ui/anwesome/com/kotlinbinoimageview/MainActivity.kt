9f86d3532b2be8aef3fdfa635e89a9f5fbdf002e
Anwesh43 <anweshthecool0@gmail.com>
2018-03-17 10:45:32 +0530

    adding fullScreen support

--- app/src/main/java/ui/anwesome/com/kotlinbinoimageview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinbinoimageview/MainActivity.kt
@@ -3,6 +3,7 @@ package ui.anwesome.com.kotlinbinoimageview
 import android.graphics.BitmapFactory
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import ui.anwesome.com.binoimageview.BinoImageView
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +11,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         BinoImageView.create(this, BitmapFactory.decodeResource(resources, R.drawable.nature_more))
+        fullScreen()
     }
+}
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
