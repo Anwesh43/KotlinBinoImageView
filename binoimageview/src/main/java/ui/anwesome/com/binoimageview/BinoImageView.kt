0d0644f6f80b1d1c25e4d21e80563cb7ab4415a7
Anwesh43 <anweshthecool0@gmail.com>
2018-03-17 09:28:03 +0530

    adding Animator class

--- binoimageview/src/main/java/ui/anwesome/com/binoimageview/BinoImageView.kt
+++ binoimageview/src/main/java/ui/anwesome/com/binoimageview/BinoImageView.kt
@@ -41,4 +41,26 @@ class BinoImageView(ctx : Context) : View(ctx) {
             }
         }
     }
+    data class Animator(var view : View, var animated : Boolean = false) {
+        fun animate(updatecb : () -> Unit) {
+            try {
+                Thread.sleep(50)
+                view.invalidate()
+            }
+            catch(ex : Exception) {
+
+            }
+        }
+        fun start() {
+            if (!animated) {
+                animated = true
+                view.postInvalidate()
+            }
+        }
+        fun stop() {
+            if (animated) {
+                animated = false
+            }
+        }
+    }
 }
