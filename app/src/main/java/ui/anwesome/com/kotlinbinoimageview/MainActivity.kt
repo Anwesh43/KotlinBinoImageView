9ff4fc858c82259cd5df3ccd49e388e819f3ca88
Anwesh43 <anweshthecool0@gmail.com>
2018-03-17 10:40:23 +0530

    adding BinoImageView in Activity

--- app/src/main/java/ui/anwesome/com/kotlinbinoimageview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinbinoimageview/MainActivity.kt
@@ -1,12 +1,14 @@
 package ui.anwesome.com.kotlinbinoimageview
 
+import android.graphics.BitmapFactory
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ui.anwesome.com.binoimageview.BinoImageView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        BinoImageView.create(this, BitmapFactory.decodeResource(resources, R.drawable.nature_more))
     }
 }
